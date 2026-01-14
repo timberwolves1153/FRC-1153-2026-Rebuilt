b489a856d9c8ebde0579caed27ec18df2a9a729a
Brian Gaffey <gaffey.brian@gmail.com>
2026-01-14 00:53:32 -0500

    Add more to git ignore

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "FRC-1153-2026-Rebuilt";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 3;
-  public static final String GIT_SHA = "b27dda688912382e33441242209bf306a173ab02";
-  public static final String GIT_DATE = "2026-01-14 00:14:34 EST";
+  public static final int GIT_REVISION = 4;
+  public static final String GIT_SHA = "cd9cdc52285ea3d6a6e6fff6e1aeefa13a6651fc";
+  public static final String GIT_DATE = "2026-01-14 00:51:14 EST";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2026-01-14 00:49:58 EST";
-  public static final long BUILD_UNIX_TIME = 1768369798317L;
+  public static final String BUILD_DATE = "2026-01-14 00:51:39 EST";
+  public static final long BUILD_UNIX_TIME = 1768369899668L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
