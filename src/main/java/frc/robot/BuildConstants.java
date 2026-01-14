cd9cdc52285ea3d6a6e6fff6e1aeefa13a6651fc
Brian Gaffey <gaffey.brian@gmail.com>
2026-01-14 00:51:14 -0500

    Update base code to 2026 Advantage Kit

---
+++ src/main/java/frc/robot/BuildConstants.java
@@ -0,0 +1,17 @@
+package frc.robot;
+
+/** Automatically generated file containing build version information. */
+public final class BuildConstants {
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "FRC-1153-2026-Rebuilt";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = 3;
+  public static final String GIT_SHA = "b27dda688912382e33441242209bf306a173ab02";
+  public static final String GIT_DATE = "2026-01-14 00:14:34 EST";
+  public static final String GIT_BRANCH = "main";
+  public static final String BUILD_DATE = "2026-01-14 00:49:58 EST";
+  public static final long BUILD_UNIX_TIME = 1768369798317L;
+  public static final int DIRTY = 1;
+
+  private BuildConstants() {}
+}
