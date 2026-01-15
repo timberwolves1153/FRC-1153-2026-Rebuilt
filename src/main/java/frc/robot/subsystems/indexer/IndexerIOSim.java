ee4657c7e09ddf062195f43a65a56f6a871da1b2
Kieran McL <98069614+KieranMcL12@users.noreply.github.com>
2026-01-15 10:01:16 -0500

    Fix code for Kraken, remove any unecessary files, and import to RobotContainer.java. Still need to set up in that file.

--- src/main/java/frc/robot/subsystems/indexer/IndexerIOSim.java
+++ src/main/java/frc/robot/subsystems/indexer/IndexerIOSim.java
@@ -1,32 +1,32 @@
-package frc.robot.subsystems.indexer;
+// package frc.robot.subsystems.indexer;
 
-import edu.wpi.first.math.MathUtil;
-import edu.wpi.first.math.system.plant.DCMotor;
-import edu.wpi.first.math.system.plant.LinearSystemId;
-import edu.wpi.first.wpilibj.simulation.DCMotorSim;
+// import edu.wpi.first.math.MathUtil;
+// import edu.wpi.first.math.system.plant.DCMotor;
+// import edu.wpi.first.math.system.plant.LinearSystemId;
+// import edu.wpi.first.wpilibj.simulation.DCMotorSim;
 
-public class IndexerIOSim implements IndexerIO {
-    private DCMotorSim sim = new DCMotorSim(
-        LinearSystemId.createDCMotorSystem(
-            DCMotor.getNEO(1),
-            1,
-            1
-        ), DCMotor.getNEO(1)
-    );
+// public class IndexerIOSim implements IndexerIO {
+//     private DCMotorSim sim = new DCMotorSim(
+//         LinearSystemId.createDCMotorSystem(
+//             DCMotor.getNEO(1),
+//             1,
+//             1
+//         ), DCMotor.getNEO(1)
+//     );
 
-    private double appliedVolts = 0.0;
+//     private double appliedVolts = 0.0;
 
-    @Override
-    public void updateInputs(IndexerIOInputs inputs) {
-        sim.setInputVoltage(appliedVolts);
-        sim.update(0.02);
+//     @Override
+//     public void updateInputs(IndexerIOInputs inputs) {
+//         sim.setInputVoltage(appliedVolts);
+//         sim.update(0.02);
 
-        inputs.appliedVolts = appliedVolts;
-        inputs.currentAmps = sim.getCurrentDrawAmps();
-    }
+//         inputs.appliedVolts = appliedVolts;
+//         inputs.currentAmps = sim.getCurrentDrawAmps();
+//     }
 
-    @Override
-    public void setVoltage(double volts) {
-        appliedVolts = MathUtil.clamp(volts, 12, -12);
-    }
-}
+//     @Override
+//     public void setVoltage(double volts) {
+//         appliedVolts = MathUtil.clamp(volts, 12, -12);
+//     }
+// }
--- src/main/java/frc/robot/subsystems/indexer/IndexerIOSparkMax.java
+++ src/main/java/frc/robot/subsystems/indexer/IndexerIOSparkMax.java
@@ -1,44 +1,44 @@
-package frc.robot.subsystems.indexer;
+// package frc.robot.subsystems.indexer;
 
-import com.revrobotics.spark.SparkMax;
-import com.revrobotics.spark.SparkBase.PersistMode;
-import com.revrobotics.spark.SparkBase.ResetMode;
-import com.revrobotics.spark.SparkLowLevel.MotorType;
-import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
-import com.revrobotics.spark.config.SparkMaxConfig;
+// import com.revrobotics.spark.SparkMax;
+// import com.revrobotics.spark.SparkBase.PersistMode;
+// import com.revrobotics.spark.SparkBase.ResetMode;
+// import com.revrobotics.spark.SparkLowLevel.MotorType;
+// import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+// import com.revrobotics.spark.config.SparkMaxConfig;
 
-public class IndexerIOSparkMax implements IndexerIO {
-    private SparkMax indexerMotor;
-    private SparkMaxConfig indexerConfig;
+// public class IndexerIOSparkMax implements IndexerIO {
+//     private SparkMax indexerMotor;
+//     private SparkMaxConfig indexerConfig;
 
-    public IndexerIOSparkMax() {
-        indexerMotor = new SparkMax(0, MotorType.kBrushless); //TODO: ID
-        indexerConfig = new SparkMaxConfig();
-    }
+//     public IndexerIOSparkMax() {
+//         indexerMotor = new SparkMax(0, MotorType.kBrushless); //TODO: ID
+//         indexerConfig = new SparkMaxConfig();
+//     }
 
-    public void configMotors() {
-        indexerConfig
-            .smartCurrentLimit(40)
-            .idleMode(IdleMode.kBrake);
+//     public void configMotors() {
+//         indexerConfig
+//             .smartCurrentLimit(40)
+//             .idleMode(IdleMode.kBrake);
 
-        indexerMotor.clearFaults();
-        indexerMotor.configure(indexerConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
-    }
+//         indexerMotor.clearFaults();
+//         indexerMotor.configure(indexerConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+//     }
 
-    @Override
-    public void updateInputs(IndexerIOInputs inputs) {
-        inputs.appliedVolts = indexerMotor.getAppliedOutput() * indexerMotor.getBusVoltage();
-        inputs.currentAmps = indexerMotor.getOutputCurrent();
-    }
+//     @Override
+//     public void updateInputs(IndexerIOInputs inputs) {
+//         inputs.appliedVolts = indexerMotor.getAppliedOutput() * indexerMotor.getBusVoltage();
+//         inputs.currentAmps = indexerMotor.getOutputCurrent();
+//     }
 
-    @Override
-    public void setVoltage(double volts) {
-        indexerMotor.setVoltage(volts);
-    }
+//     @Override
+//     public void setVoltage(double volts) {
+//         indexerMotor.setVoltage(volts);
+//     }
 
-    @Override
-    public void stop() {
-        indexerMotor.setVoltage(0);
-    }
+//     @Override
+//     public void stop() {
+//         indexerMotor.setVoltage(0);
+//     }
 
-}
+// }
