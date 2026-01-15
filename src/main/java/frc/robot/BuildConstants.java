4823deecfae20833fe79e30cfbc3e91e561a7eb5
walpolerobotics <roborebels1153@gmail.com>
2026-01-14 19:20:01 -0500

    Make this work on a school laptop

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "FRC-1153-2026-Rebuilt";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 4;
-  public static final String GIT_SHA = "cd9cdc52285ea3d6a6e6fff6e1aeefa13a6651fc";
-  public static final String GIT_DATE = "2026-01-14 00:51:14 EST";
+  public static final int GIT_REVISION = 6;
+  public static final String GIT_SHA = "76741fced2c5e454b69fd21772d2e0867fc7bad6";
+  public static final String GIT_DATE = "2026-01-14 00:56:12 EST";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2026-01-14 00:51:39 EST";
-  public static final long BUILD_UNIX_TIME = 1768369899668L;
+  public static final String BUILD_DATE = "2026-01-14 19:19:08 EST";
+  public static final long BUILD_UNIX_TIME = 1768436348492L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
