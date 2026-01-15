f420f399a1fd63df4ab7edba130e8dedae10663f
Tsingh1153 <tanayps2008@gmail.com>
2026-01-14 19:30:13 -0500

    branch test

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,13 +5,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "FRC-1153-2026-Rebuilt";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 7;
-  public static final String GIT_SHA = "4823deecfae20833fe79e30cfbc3e91e561a7eb5";
-  public static final String GIT_DATE = "2026-01-14 19:20:01 EST";
-  public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2026-01-14 19:25:15 EST";
-  public static final long BUILD_UNIX_TIME = 1768436715334L;
-  public static final int DIRTY = 1;
+  public static final int GIT_REVISION = 8;
+  public static final String GIT_SHA = "f1f357f303dc70073d0c753deefa68820a1f2284";
+  public static final String GIT_DATE = "2026-01-14 19:27:46 EST";
+  public static final String GIT_BRANCH = "Climber";
+  public static final String BUILD_DATE = "2026-01-14 19:29:13 EST";
+  public static final long BUILD_UNIX_TIME = 1768436953297L;
+  public static final int DIRTY = 0;
 
   private BuildConstants() {}
 }
