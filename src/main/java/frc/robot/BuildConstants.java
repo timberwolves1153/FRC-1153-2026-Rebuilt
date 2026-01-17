bbc07f548b47eea4ab09f8ab99e29c276d657853
Kieran McL <98069614+KieranMcL12@users.noreply.github.com>
2026-01-17 13:46:20 -0500

    Fix

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -1,17 +1,19 @@
 package frc.robot;
 
-/** Automatically generated file containing build version information. */
+/**
+ * Automatically generated file containing build version information.
+ */
 public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "FRC-1153-2026-Rebuilt";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 6;
-  public static final String GIT_SHA = "76741fced2c5e454b69fd21772d2e0867fc7bad6";
-  public static final String GIT_DATE = "2026-01-14 00:56:12 EST";
-  public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2026-01-14 19:19:08 EST";
-  public static final long BUILD_UNIX_TIME = 1768436348492L;
-  public static final int DIRTY = 1;
+  public static final int GIT_REVISION = 13;
+  public static final String GIT_SHA = "f009717467ffe8f37f17fc413078f3ff8a18b6f9";
+  public static final String GIT_DATE = "2026-01-17 13:31:50 EST";
+  public static final String GIT_BRANCH = "Indexer";
+  public static final String BUILD_DATE = "2026-01-17 13:45:25 EST";
+  public static final long BUILD_UNIX_TIME = 1768675525560L;
+  public static final int DIRTY = 0;
 
-  private BuildConstants() {}
+  private BuildConstants(){}
 }
