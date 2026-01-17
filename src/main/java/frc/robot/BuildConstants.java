74d2ed1177fd3112eb0f3802ec9538b84b9e91b8
Kieran McL <98069614+KieranMcL12@users.noreply.github.com>
2026-01-17 13:50:22 -0500

    Try to fix

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "FRC-1153-2026-Rebuilt";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 14;
-  public static final String GIT_SHA = "bbc07f548b47eea4ab09f8ab99e29c276d657853";
-  public static final String GIT_DATE = "2026-01-17 13:46:20 EST";
+  public static final int GIT_REVISION = 15;
+  public static final String GIT_SHA = "a603d88ae162ecf148aac9e6ce8f60fd9aa00c7d";
+  public static final String GIT_DATE = "2026-01-17 13:48:26 EST";
   public static final String GIT_BRANCH = "Indexer";
-  public static final String BUILD_DATE = "2026-01-17 13:48:02 EST";
-  public static final long BUILD_UNIX_TIME = 1768675682603L;
-  public static final int DIRTY = 1;
+  public static final String BUILD_DATE = "2026-01-17 13:49:02 EST";
+  public static final long BUILD_UNIX_TIME = 1768675742377L;
+  public static final int DIRTY = 0;
 
   private BuildConstants(){}
 }
