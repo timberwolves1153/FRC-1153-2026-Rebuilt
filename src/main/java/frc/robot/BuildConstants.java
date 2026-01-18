d7e9296d7149f646300bd80e0063202329633a08
Brian Gaffey <gaffey.brian@gmail.com>
2026-01-18 11:51:51 -0500

    Update to wpi lib 2.1

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "FRC-1153-2026-Rebuilt";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 7;
-  public static final String GIT_SHA = "4823deecfae20833fe79e30cfbc3e91e561a7eb5";
-  public static final String GIT_DATE = "2026-01-14 19:20:01 EST";
+  public static final int GIT_REVISION = 11;
+  public static final String GIT_SHA = "9feb328c939dd3cf1b667ced7edba6dd524c07e4";
+  public static final String GIT_DATE = "2026-01-17 15:33:59 EST";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2026-01-17 15:31:22 EST";
-  public static final long BUILD_UNIX_TIME = 1768681882608L;
+  public static final String BUILD_DATE = "2026-01-18 11:50:46 EST";
+  public static final long BUILD_UNIX_TIME = 1768755046263L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
