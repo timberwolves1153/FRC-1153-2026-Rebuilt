d5481fff20be81600b7eeb9e8b21ad6f68c9772d
Brian Gaffey <gaffey.brian@gmail.com>
2026-01-18 18:35:06 -0500

    Update BuildConstants.java

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,13 +5,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "FRC-1153-2026-Rebuilt";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 12;
-  public static final String GIT_SHA = "d7e9296d7149f646300bd80e0063202329633a08";
-  public static final String GIT_DATE = "2026-01-18 11:51:51 EST";
+  public static final int GIT_REVISION = 20;
+  public static final String GIT_SHA = "dc2b2fc5989e6dff20d446bd3a4117a413e3b683";
+  public static final String GIT_DATE = "2026-01-18 18:34:18 EST";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2026-01-18 18:34:13 EST";
-  public static final long BUILD_UNIX_TIME = 1768779253519L;
-  public static final int DIRTY = 1;
+  public static final String BUILD_DATE = "2026-01-18 18:34:30 EST";
+  public static final long BUILD_UNIX_TIME = 1768779270920L;
+  public static final int DIRTY = 0;
 
   private BuildConstants() {}
 }
