32a8c272d281abd92500e601e86d04e2185f3e5b
walpolerobotics <roborebels1153@gmail.com>
2026-01-29 21:47:58 -0500

    testing

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "FRC-1153-2026-Rebuilt";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 59;
-  public static final String GIT_SHA = "c6b3b045e76a3d01fea33d37339851cdf1dd5d54";
-  public static final String GIT_DATE = "2026-01-28 18:28:28 EST";
+  public static final int GIT_REVISION = 60;
+  public static final String GIT_SHA = "152bf78c5c982cc5be79aec8beea08cc43fb668b";
+  public static final String GIT_DATE = "2026-01-29 19:54:00 EST";
   public static final String GIT_BRANCH = "Vision";
-  public static final String BUILD_DATE = "2026-01-29 19:40:11 EST";
-  public static final long BUILD_UNIX_TIME = 1769733611400L;
+  public static final String BUILD_DATE = "2026-01-29 21:26:57 EST";
+  public static final long BUILD_UNIX_TIME = 1769740017542L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
---
+++ src/main/java/frc/robot/subsystems/launcher/flywheel/Flywheel.java
@@ -0,0 +1,57 @@
+// package frc.robot.subsystems.launcher.flywheel;
+
+// import edu.wpi.first.math.controller.PIDController;
+// import edu.wpi.first.math.controller.SimpleMotorFeedforward;
+// import edu.wpi.first.wpilibj2.command.SubsystemBase;
+// import frc.robot.Constants;
+// import org.littletonrobotics.junction.Logger;
+
+// public class Flywheel extends SubsystemBase {
+//   private final FlywheelIO io;
+//   private final FlywheelIOInputsAutoLogged inputs = new FlywheelIOInputsAutoLogged();
+
+//   private PIDController flywheelPID;
+//   private SimpleMotorFeedforward flywheelFF;
+
+//   public Flywheel(FlywheelIO flywheelIO) {
+//     io = flywheelIO;
+
+//     switch (Constants.currentMode) {
+//       case REAL:
+//       case REPLAY:
+//         break;
+
+//       case SIM:
+//         break;
+//     }
+
+//     flywheelPID = new PIDController(0.1, 0, 0);
+//     flywheelFF = new SimpleMotorFeedforward(0, 0.0075); // TODO: Tune
+//   }
+
+//   @Override
+//   public void periodic() {
+//     io.updateInputs(inputs);
+//     Logger.processInputs("Flywheel", inputs);
+//   }
+
+//   public void setVoltageLeader(double volts) {
+//     io.setVoltageLeader(volts);
+//   }
+
+//   public void setVoltageFollower(double volts) {
+//     io.setVoltageFollower(volts);
+//   }
+
+//   public void setVelocityLeader(double velocity) {
+//     io.setVoltageLeader(velocity);
+//   }
+
+//   public void setVelocityFollower(double velocity) {
+//     io.setVoltageFollower(velocity);
+//   }
+
+//   public void stopFlywheel() {
+//     io.stopFlywheel();
+//   }
+// }
