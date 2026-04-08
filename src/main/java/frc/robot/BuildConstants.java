e09c65ec3d5f76d2fd5c4f6d42596e9558821726
Cole <cwhoward09@gmail.com>
2026-04-07 21:40:07 -0400

    more autos stuff

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "FRC-1153-2026-Rebuilt";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 125;
-  public static final String GIT_SHA = "f9eeeb5f674e09ad788bb142e72f24fa2ddeaee9";
-  public static final String GIT_DATE = "2026-04-06 18:09:26 EDT";
+  public static final int GIT_REVISION = 126;
+  public static final String GIT_SHA = "43ed3cb6e3c917d02ed5259456bedcda7a664a6d";
+  public static final String GIT_DATE = "2026-04-06 22:13:12 EDT";
   public static final String GIT_BRANCH = "Comp-Bot";
-  public static final String BUILD_DATE = "2026-04-06 21:36:46 EDT";
-  public static final long BUILD_UNIX_TIME = 1775525806638L;
+  public static final String BUILD_DATE = "2026-04-07 20:57:25 EDT";
+  public static final long BUILD_UNIX_TIME = 1775609845976L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
