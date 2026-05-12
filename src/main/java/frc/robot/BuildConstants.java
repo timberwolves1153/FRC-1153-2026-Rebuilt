9f4f9e57e1a50759ec694c10796e5d894e7fe0d8
Cole <cwhoward09@gmail.com>
2026-05-11 20:09:33 -0400

    SOTM auto improvements

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "FRC-1153-2026-Rebuilt";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 137;
-  public static final String GIT_SHA = "b5b7388e27d680f368aaff78083ce9e0b91f2e02";
-  public static final String GIT_DATE = "2026-05-04 19:27:53 EDT";
+  public static final int GIT_REVISION = 138;
+  public static final String GIT_SHA = "29a94e6324868f7fa2bec4c1d5c818166fe7584b";
+  public static final String GIT_DATE = "2026-05-04 20:03:02 EDT";
   public static final String GIT_BRANCH = "Comp-Bot";
-  public static final String BUILD_DATE = "2026-05-04 19:49:26 EDT";
-  public static final long BUILD_UNIX_TIME = 1777938566394L;
+  public static final String BUILD_DATE = "2026-05-11 19:45:56 EDT";
+  public static final long BUILD_UNIX_TIME = 1778543156290L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
